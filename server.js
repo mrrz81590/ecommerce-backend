e19db0cd333d2b4c5c3331d990ef8452c1fdd740
mrrz81590 <mr.rz81590@gmail.com>
2026-01-11 01:34:02 +0530

    connect product routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/products", require("./routes/productRoutes"));
 
 // test route
 app.get("/", (req, res) => {
